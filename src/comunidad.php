8ac8d55183fb35fc7d8f1727257eec53675c8bc3
Jon Latorre <moebius@etxea.net>
2014-09-19 15:44:50 +0200

    mostrando mas datos

--- src/comunidad.php
+++ src/comunidad.php
@@ -23,6 +23,60 @@ $comunidad->get('/seguros/', function () use ($app) {
 ->bind('comunidad_seguro')
 ;
 
+//Vista que muestra el HTML
+$comunidad->get('/cargos/', function () use ($app) {
+    return $app['twig']->render('comunidad/cargos.html');
+})->bind('comunidad_cargos');
+//Vista que muestra el JSON para el datagrid
+$comunidad->match('/{id}/cargos/json/', function ($id) use ($app) {
+    $cargos = $app['db']->fetchAll('SELECT * FROM tpasadas WHERE idcomunidad = ?', array($id));;
+    return $app->json($cargos);
+})
+->bind('comunidad_cargos_json')
+->assert('id', '\d+'); //nos aseguramos que nos pasan un decimal
+
+
+//Vista que muestra el HTML
+$comunidad->get('/convocatorias/', function () use ($app) {
+    return $app['twig']->render('comunidad/convocatorias.html');
+})->bind('comunidad_convocatorias');
+//Vista que muestra el JSON para el datagrid
+$comunidad->match('/{id}/convocatorias/json/', function ($id) use ($app) {
+    $convocatorias = $app['db']->fetchAll('SELECT * FROM tconvocatorias WHERE idcomunidad = ?', array($id));;
+    return $app->json($convocatorias);
+})
+->bind('comunidad_convocatorias_json')
+->assert('id', '\d+'); //nos aseguramos que nos pasan un decimal
+
+
+//Vista que muestra el HTML
+$comunidad->get('/acuerdos/', function () use ($app) {
+    return $app['twig']->render('comunidad/acuerdos.html');
+})->bind('comunidad_acuerdos');
+//Vista que muestra el JSON para el datagrid
+$comunidad->match('/{id}/acuerdos/json/', function ($id) use ($app) {
+    $acuerdos = $app['db']->fetchAll('SELECT * FROM tacuerdos WHERE idcomunidad = ?', array('112'));;
+    return $app->json($acuerdos);
+})
+->bind('comunidad_acuerdos_json')
+->assert('id', '\d+'); //nos aseguramos que nos pasan un decimal
+
+
+//Vista que muestra el HTML
+$comunidad->get('/proveedores/', function () use ($app) {
+    return $app['twig']->render('comunidad/proveedores.html');
+})->bind('comunidad_proveedores');
+//Vista que muestra el JSON para el datagrid
+$comunidad->match('/{id}/proveedores/json/', function ($id) use ($app) {
+    $proveedores = $app['db']->fetchAll('SELECT * from tproveedores JOIN tproveedorescomunidad ON tproveedorescomunidad.idproveedor = tproveedores.id WHERE tproveedorescomunidad.idcomunidad = ?', array($id));;
+    return $app->json($proveedores);
+})
+->bind('comunidad_proveedores_json')
+->assert('id', '\d+'); //nos aseguramos que nos pasan un decimal
+
+
+
+
 $comunidad->match('/{id}/propietarios/json/', function ($id) use ($app) {
     $propietarios = $app['db']->fetchAll('SELECT * FROM tpropietarios WHERE idcomunidad = ?', array($id));
     return $app->json($propietarios);
